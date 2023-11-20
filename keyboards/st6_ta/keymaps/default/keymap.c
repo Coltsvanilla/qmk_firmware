9d37123d5150d5f5964b3ad8dd01c55b83981e9a
T.Nishidomi <kayanaku245@gmail.com>
2023-11-20 09:50:22 +0900

    st6_ta　ファームウェア作成

---
+++ keyboards/st6_ta/keymaps/default/keymap.c
@@ -0,0 +1,22 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,
+        KC_G,   KC_H,   KC_J,   KC_K,   KC_L
+    )
+};
